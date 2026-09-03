f04caddd0c9f4dbff948d7de604aa448d1460ef1
diyfirst9-jpg <diyfirst9@gmail.com>
2026-09-03 20:41:32 +0700

    Update RenderableWindow.java

--- app/src/main/runtime/display/renderer/RenderableWindow.java
+++ app/src/main/runtime/display/renderer/RenderableWindow.java
@@ -1,16 +1,13 @@
 package com.winlator.cmod.runtime.display.renderer;
 
 import com.winlator.cmod.runtime.display.xserver.Drawable;
-import com.winlator.cmod.runtime.display.xserver.Window;
 
 class RenderableWindow {
   final Drawable content;
-  final Window window;
   short rootX;
   short rootY;
 
-  public RenderableWindow(Window window, Drawable content, int rootX, int rootY) {
-    this.window = window;
+  public RenderableWindow(Drawable content, int rootX, int rootY) {
     this.content = content;
     this.rootX = (short) rootX;
     this.rootY = (short) rootY;
